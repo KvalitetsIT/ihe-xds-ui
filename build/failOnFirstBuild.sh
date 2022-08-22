f4b66dd401a3ec96389a7591ec5eabcc2a2c9834
Mikkel Birch <mib@kvalitetsit.dk>
2022-08-22 13:50:10 +0200

    after setup.sh

--- build/failOnFirstBuild.sh
+++ build/failOnFirstBuild.sh
@@ -2,7 +2,7 @@
 
 echo "${GITHUB_REPOSITORY}"
 echo "${DOCKER_SERVICE}"
-if [ "${GITHUB_REPOSITORY}" != "kvalitetsit/kittyact" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/kittyact" ]; then
+if [ "${GITHUB_REPOSITORY}" != "kvalitetsit/ihe-xds" ] && [ "${DOCKER_SERVICE}" = "kvalitetsit/ihe-xds" ]; then
   echo "Please run setup.sh REPOSITORY_NAME"
   exit 1
 fi
