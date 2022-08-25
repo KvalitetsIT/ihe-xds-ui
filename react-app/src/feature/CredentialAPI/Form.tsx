8bd9383b77273fc3304bd60a2a6709c201408f34
Elarsen14 <eml@kvalitetsit.dk>
2022-08-25 09:58:15 +0200

    quick push

--- react-app/src/feature/CredentialAPI/Form.tsx
+++ react-app/src/feature/CredentialAPI/Form.tsx
@@ -1,45 +1,17 @@
-import { useGetIDsforOwnerQuery } from "./redux/CredentialInfoApiSlice"
-import Loading from "../../components/loading"
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import Row1 from "./FormRows/Row1"
+
 
 
 
 function Form(props : any) {
-    const {data , isLoading, isSuccess, isError, error} = useGetIDsforOwnerQuery(props.sessionID)
-    const navigate = useNavigate()
     
-    /*const [owners, setOwners] = useState([]);
-    
-    const updateOwnsers = () => {
-
-        for (let obj of data) {
-            let temp = {value : obj, label : obj}
-            setOwners((prev) => ({...prev, temp}))
-        }
-        
-    }*/
-
-
-    if (isLoading) {
-        return Loading()
-    }
-    else if (isSuccess) {
-        return (
-            <>
-            <select>
-                {data!.map((item : any, index : number) => {
-                    return (
-                        <option key={index} value={"" + item.id}>{item.id}</option>
-                    )
-                })}
-
-            </select>
-            <a onClick={() => navigate("/upload-certifacte")}>Upload own certificate</a>
-            </>        
-        )
-    }
-    return null
+    return (
+        <div>
+            <form method="POST">
+                <Row1 />
+            </form>
+        </div>
+    )
 
 
 
